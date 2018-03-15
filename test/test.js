0abe5a0fdfe5aa3eac08576f6caf14ce68e4c06d
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-03-15 16:56:20 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -20,10 +20,10 @@ if (process.env.CI === true) {
   } /* istanbul ignore next */ else if (platform === 'linux') {
     pathTo = path.join(require('os').homedir(), '.steam')
   } else if (platform === 'win32' && arch === 'x86') {
-      pathTo = path.join('C:', 'Program Files (x86)', 'Steam')
+    pathTo = path.join('C:', 'Program Files (x86)', 'Steam')
   } else {
-      pathTo = path.join('C:', 'Program Files', 'Steam')
-  } 
+    pathTo = path.join('C:', 'Program Files', 'Steam')
+  }
 } else {
   pathTo = path.join(__dirname, 'Dummy')
 }
